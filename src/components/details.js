fa0c9e9c5a4486f13a14abeb500a6bc4b5945d29
James Jager <jviper882000@gmail.com>
2020-08-11 03:18:13 -0400

    built the details component for quicklinks

---
+++ src/components/details.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Details extends Component {
+  render() {
+    const { title, links } = this.props;
+    return (
+      <div className="details">
+        <div className="details__title">{title}</div>
+        <div className="details__links">
+          {links.map((link) => {
+            return (
+              <a
+                key={link._id}
+                onClick={link.onClick}
+                div
+                className="details__link"
+              >
+                {link.title}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Details;
