04651eef976bb181bcea93a34c4b84210f420b9e
James Jager <jviper882000@gmail.com>
2020-08-16 17:39:15 -0400

    developed shopReducer and fetch shop categories, action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,13 @@ import {
 
 import { fetchUserPurchases, setPuchaseDetail } from "./user";
 
+import { fetchShopCategories } from "./shop";
+
 export {
   setHeaderLinks,
   setNavbarLinks,
   changeNavbarActive,
   fetchUserPurchases,
   setPuchaseDetail,
+  fetchShopCategories,
 };
